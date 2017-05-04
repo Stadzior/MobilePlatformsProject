5c2c4f066444ad267834959716a631121337c0af
Stadzior <Stadzior@gmail.com>
2017-05-04 15:15:53 +0200

    Asynchronious Task done.

--- MobilePlatformsProject/MobilePlatformsProject/MainPage.xaml.cs
+++ MobilePlatformsProject/MobilePlatformsProject/MainPage.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -22,9 +24,37 @@ namespace MobilePlatformsProject
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private int _counter;
+        private CancellationTokenSource _tokenSource = new CancellationTokenSource();
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private async void btnRun_Click(object sender, RoutedEventArgs e)
+        {
+            await Task.Factory.StartNew(async () =>
+            {
+                for (int i = 0; i < 1000000; i++)
+                {
+                    if (!_tokenSource.IsCancellationRequested)
+                    {
+                        _counter++;
+                        await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () => textBoxCounter.Text = _counter.ToString());
+                        await Task.Delay(1000);
+                    }
+                }
+            },_tokenSource.Token);
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            _tokenSource.Cancel();
+        }
+
+        private void btnHello_Click(object sender, RoutedEventArgs e)
+        {
+            textBoxHello.Text = $"Hello {_counter} time!";
         }
     }
 }
